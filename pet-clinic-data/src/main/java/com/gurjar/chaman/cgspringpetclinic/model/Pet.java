bf145b28750036f96be215cfbe431b490118baa2
ChamanGurjar <chamangujjar162@gmail.com>
2020-07-26 22:34:43 +0530

    tags (author and file created on) related changes

--- pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/model/Pet.java
@@ -3,10 +3,9 @@ package com.gurjar.chaman.cgspringpetclinic.model;
 import java.time.LocalDate;
 
 /**
- * @Author: Chaman Gurjar
- * @Date : 26-Jul-2020
+ * @author - Chaman Gurjar
+ * @since  - 26-Jul-2020
  */
-
 public class Pet {
     private PetType petType;
     private Owner owner;
