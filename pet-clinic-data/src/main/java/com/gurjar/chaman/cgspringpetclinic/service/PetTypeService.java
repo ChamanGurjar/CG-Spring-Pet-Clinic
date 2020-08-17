a6a06d4291a64046f7da64e1c66fb46a5516587f
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-17 21:15:32 +0530

    PetType Service and PetTypeMapService implemented. Closes #29 .

---
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/service/PetTypeService.java
@@ -0,0 +1,11 @@
+package com.gurjar.chaman.cgspringpetclinic.service;
+
+import com.gurjar.chaman.cgspringpetclinic.model.PetType;
+
+/**
+ * @author - Chaman Gurjar
+ * @version - 1.0.0 - 17-Aug-2020
+ */
+
+public interface PetTypeService extends BaseService<PetType, Long> {
+}
