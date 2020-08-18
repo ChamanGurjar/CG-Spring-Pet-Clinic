95d4e7855ebb464ddb33ccbb9af8b396d54f8204
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-18 07:55:46 +0530

    Added Speciality Map Service impl. Closes #30.

---
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/service/SpecialitiesService.java
@@ -0,0 +1,12 @@
+package com.gurjar.chaman.cgspringpetclinic.service;
+
+import com.gurjar.chaman.cgspringpetclinic.model.Speciality;
+
+/**
+ * @author - Chaman Gurjar
+ * @version - 1.0.0 - 18-Aug-2020
+ */
+
+public interface SpecialitiesService  extends  BaseService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/service/map/SpecialityMapService.java
@@ -0,0 +1,41 @@
+package com.gurjar.chaman.cgspringpetclinic.service.map;
+
+import com.gurjar.chaman.cgspringpetclinic.model.Speciality;
+import com.gurjar.chaman.cgspringpetclinic.service.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author - Chaman Gurjar
+ * @version - 1.0.0 - 18-Aug-2020
+ */
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public  void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+}
