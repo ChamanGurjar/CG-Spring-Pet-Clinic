41207ae91379a27b760fe28a7adcb437a7a79672
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-19 08:44:50 +0530

    Vet and Speciality classes converted to entity and ManyToMany mapping is setup. Closes #37.

--- pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/model/Speciality.java
@@ -1,10 +1,15 @@
 package com.gurjar.chaman.cgspringpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author - Chaman Gurjar
  * @version - 1.0.0 - 17-Aug-2020
  */
 
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
     private String description;
--- pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package com.gurjar.chaman.cgspringpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,9 +9,15 @@ import java.util.Set;
  * @since  - 26-Jul-2020
  */
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
- private Set<Speciality> specialities = new HashSet<>();
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+    private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
         return specialities;
