20f45154cad73217acaf5e954fee56cb97757a9b
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-19 08:33:46 +0530

    BaseEntity converted to Jpa and annotated as MappedSuperclass. Closes #35.

--- pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/model/BaseEntity.java
@@ -1,12 +1,20 @@
 package com.gurjar.chaman.cgspringpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  * @author - Chaman Gurjar
  * @version - 1.0.0 - 02-Aug-2020
  */
 
+@MappedSuperclass
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     //    Getters And Setters
