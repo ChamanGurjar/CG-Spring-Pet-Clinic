6cbe613d904b08e4bf5bc5f863f361bd7bc390e1
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-19 09:21:28 +0530

    Owner Spring data jpa service implemented. Closes #40.

---
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/service/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,57 @@
+package com.gurjar.chaman.cgspringpetclinic.service.springdatajpa;
+
+import com.gurjar.chaman.cgspringpetclinic.model.Owner;
+import com.gurjar.chaman.cgspringpetclinic.repositories.OwnerRepository;
+import com.gurjar.chaman.cgspringpetclinic.service.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author - Chaman Gurjar
+ * @version - 1.0.0 - 19-Aug-2020
+ */
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository) {
+        this.ownerRepository = ownerRepository;
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return ownerRepository.findByLastName(lastName);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return ownerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        ownerRepository.deleteById(id);
+    }
+}
