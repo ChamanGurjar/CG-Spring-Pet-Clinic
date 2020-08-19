092c7b04541b657d58c1276af58867b3d0b1c7a3
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-19 09:38:12 +0530

    PetType Spring data jpa service implemented. Closes #41.

---
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/service/springdatajpa/PetTypeDJpaService.java
@@ -0,0 +1,53 @@
+package com.gurjar.chaman.cgspringpetclinic.service.springdatajpa;
+
+import com.gurjar.chaman.cgspringpetclinic.model.PetType;
+import com.gurjar.chaman.cgspringpetclinic.repositories.PetTypeRepository;
+import com.gurjar.chaman.cgspringpetclinic.service.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author - Chaman Gurjar
+ * @version - 1.0.0 - 19-Aug-2020
+ */
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
