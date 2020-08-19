77c5a786f5b2813008cb0ad208752cfe47cddeda
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-19 21:01:10 +0530

    Speciality SD Jpa Service Created. Closes #44.

---
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/service/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,53 @@
+package com.gurjar.chaman.cgspringpetclinic.service.springdatajpa;
+
+import com.gurjar.chaman.cgspringpetclinic.model.Speciality;
+import com.gurjar.chaman.cgspringpetclinic.repositories.SpecialityRepository;
+import com.gurjar.chaman.cgspringpetclinic.service.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author - Chaman Gurjar
+ * @version - 1.0.0 - 19-Aug-2020
+ */
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return specialityRepository.save(speciality);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        specialityRepository.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
