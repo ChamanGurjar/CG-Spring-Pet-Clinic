d8e9cc22d39ed184f3d25a363b001c9550815609
ChamanGurjar <chamangujjar162@gmail.com>
2020-08-19 21:22:39 +0530

    Visit Map Service implemented. Closes #46.

---
+++ pet-clinic-data/src/main/java/com/gurjar/chaman/cgspringpetclinic/service/map/VisitMapService.java
@@ -0,0 +1,44 @@
+package com.gurjar.chaman.cgspringpetclinic.service.map;
+
+import com.gurjar.chaman.cgspringpetclinic.model.Visit;
+import com.gurjar.chaman.cgspringpetclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author - Chaman Gurjar
+ * @version - 1.0.0 - 19-Aug-2020
+ */
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+        || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException(("Invalid Visit"));
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+}
